afa822843b28a4296b2540f9dcecc210a56e3fc4
Johnson Lin <johnson401.cs09@nycu.edu.tw>
2024-07-24 23:08:51 +0800

    Carousel complete

--- ga.py
+++ ga.py
@@ -7,7 +7,7 @@ from flask import Flask, request
 # Line message APIs
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ButtonsTemplate, LocationAction, LocationMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ButtonsTemplate, LocationAction, LocationMessage, CarouselTemplate
 from linebot.models import * 
 from linebot.v3.messaging import MessagingApi
 
@@ -38,8 +38,9 @@ def handle_location_message(event):
     address = event.message.address
 
     # 在這裡可以處理使用者分享位置資訊的相關邏輯，例如存入資料庫、進行位置相關的運算等
-    reply = f"您分享的位置資訊：\n緯度: {lat}\n經度: {lng}\n地址: {address}"
-    reply = reply + "\n接下來您可依據該地點尋找鄰近餐廳！"
+    reply = f"搜尋位置設定：{address}\n"
+    reply += f"最低可接受評價：{star}\n"
+    reply += "***  設定完成，請開始搜尋！***"
 
     # 回覆訊息給使用者
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
@@ -85,6 +86,7 @@ def handle_rich_menu_change(event):
     global star
     if event.postback.data[:4] == 'star':
         star = int(event.postback.data[4])
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f'可接受評價下限: {star}'))
     elif event.postback.data[:3] == 'cat':
         global menu_category
         if event.postback.data[4:] == 'add':
@@ -108,23 +110,6 @@ def handle_postback(event):
         get_location(event)
         return
 
-    # For testing ----------------------------
-    # address = "苗栗市玉清路"
-    # if address == '':
-    #     get_location(event)
-    #     return
-    # # target_type is the type of locations for google map to search
-    # # e.g.: restaurant, cafe, convenience_store
-    # target_type = event.postback.data
-    #
-    # addurl = 'https://maps.googleapis.com/maps/api/geocode/json?key={}&address={}&sensor=false'.format(GOOGLE_API_KEY, address)
-    # addressReq = requests.get(addurl)
-    # addressDoc = addressReq.json()
-    # lat = addressDoc['results'][0]['geometry']['location']['lat']
-    # lng = addressDoc['results'][0]['geometry']['location']['lng']
-    # For testing ----------------------------
-
-    # <<<<
     if address == '':
         get_location(event)
         return
@@ -133,7 +118,6 @@ def handle_postback(event):
     addurl = 'https://maps.googleapis.com/maps/api/geocode/json?key={}&address={}&sensor=false'.format(GOOGLE_API_KEY, address)
     addressReq = requests.get(addurl)
     addressDoc = addressReq.json()
-    # <<<<
 
     foodStoreSearch = ""
     if target_type in {'restaurant', 'cafe', 'convenience_store'}:
@@ -166,51 +150,56 @@ def handle_postback(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
         return
 
-    restaurant = top20_restaurants[random.choice(bravo)]
+    selected_restaurants = []
+    random.shuffle(top20_restaurants)
+    for restaurant in top20_restaurants:
+        if restaurant.get("photos") is not None and len(restaurant['name']) < 40:
+            selected_restaurants.append(restaurant)
+            if len(selected_restaurants) == 10:
+                break
 
-    if restaurant.get("photos") is None:
-        thumbnail_image_url = None
-    else:
-        photo_reference = restaurant["photos"][0]["photo_reference"]
-        photo_width = restaurant["photos"][0]["width"]
-        thumbnail_image_url = f"https://maps.googleapis.com/maps/api/place/photo?key={GOOGLE_API_KEY}&photoreference={photo_reference}&maxwidth={photo_width}"
+    carousel_template = CarouselTemplate()
+    for restaurant in selected_restaurants:
+    # restaurant = top20_restaurants[random.choice(bravo)]
+        if restaurant.get("photos") is None:
+            thumbnail_image_url = None
+        else:
+            photo_reference = restaurant["photos"][0]["photo_reference"]
+            photo_width = restaurant["photos"][0]["width"]
+            thumbnail_image_url = f"https://maps.googleapis.com/maps/api/place/photo?key={GOOGLE_API_KEY}&photoreference={photo_reference}&maxwidth={photo_width}"
 
-    # Assemble reply message
-    rating = "無" if restaurant.get ("rating") is None else restaurant[ "rating"]
-    address="沒有資料" if restaurant.get("vicinity") is None else restaurant["vicinity"]
-    if address == "沒有資料":
-        address = restaurant["formatted_address"]
-    details = "Google Map評分:{}\n地址:{}".format(rating, address)
-    # google map url for restaurants
-    map_url = f"https://www.google.com/maps/search/?api=1&query={restaurant['geometry']['location']['lat']},{restaurant['geometry']['location']['lng']}&query_place_id={restaurant['place_id']}"
+        # Assemble reply message
+        rating = "無" if restaurant.get ("rating") is None else restaurant[ "rating"]
+        address="沒有資料" if restaurant.get("vicinity") is None else restaurant["vicinity"]
+        if address == "沒有資料":
+            address = restaurant["formatted_address"]
+        details = "Google Map評分:{}\n地址:{}".format(rating, address)
+        # google map url for restaurants
+        map_url = f"https://www.google.com/maps/search/?api=1&query={restaurant['geometry']['location']['lat']},{restaurant['geometry']['location']['lng']}&query_place_id={restaurant['place_id']}"
 
-    # line reply block template
-    buttons_template = TemplateSendMessage(
-    alt_text=restaurant["name"],
-    template=ButtonsTemplate(
-        thumbnail_image_url=thumbnail_image_url,
-        title=restaurant['name'],
-        text=details,
-        actions=[
-            URITemplateAction(
-                label='查看地圖',
-                uri=map_url
+        column = CarouselColumn(
+            thumbnail_image_url=thumbnail_image_url,
+            title=restaurant['name'],
+            text=details,
+            actions=[
+                MessageAction(
+                    label='Open in Google Map',
+                    text=map_url
                 )
             ]
         )
-    )
-    # reply
-    line_bot_api.reply_message(event.reply_token, buttons_template)
+        carousel_template.columns.append(column)
 
+    template_message = TemplateSendMessage(
+        alt_text='Carousel template',
+        template=carousel_template
+    )
+    line_bot_api.reply_message(event.reply_token, template_message)
 
 @app.route("/", methods=['POST'])
 def linebot():
     body = request.get_data(as_text=True)
     signature = request.headers['X-Line-Signature']
-
-    print("BODY")
-    print(body)
-    print()
 
     try:
         handler.handle(body, signature)
@@ -235,6 +224,3 @@ def handle_location_message_event(event):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
-
-# access_token = 'DDOS1nJL2oxe7nmekZ0OhZTFSIG0RbR9RdOnM9wJnGBfQlrmvndAMt5khcI+0aTOHTh4iepuG0LPTTEhfWyChjUtmy/ARuqC5ga9DcD0LZOYrxmrmGYooplhWRWjOeB+k0ikq6gy0fBGPEr9rkCVCwdB04t89/1O/w1cDnyilFU='
-# secret = 'e90e4acf2cce57e39e9c69032f23ea22'
